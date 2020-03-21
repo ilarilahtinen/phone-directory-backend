ab1899675cd85f7fdfc9c02c5f12179f9d80bc74
ilari <ilari.k.lahtinen@gmail.com>
2020-03-21 11:24:15 +0200

    add frontend deployment

---
+++ build/precache-manifest.afb653fc482f289916a41f8594e11945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a311969157824f8e6b45b6e067201f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc00ab5d065cfbff8183",
+    "url": "/static/js/2.0c536ee6.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/static/js/2.0c536ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64427e99021e51ff2121",
+    "url": "/static/js/main.87b85f1d.chunk.js"
+  },
+  {
+    "revision": "8a5fc02e3f6d69b6b7aa",
+    "url": "/static/js/runtime-main.f87c72b2.js"
+  }
+]);
